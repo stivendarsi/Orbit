075a036cfb4e78fe64fce80d2f2776c9e3c62442
stiven <stivendaris9@gmail.com>
2026-07-01 01:05:53 +0300

    Patches

--- src/main/java/me/stivendarsi/orbit/orbit/menus/QuestMenu.java
+++ src/main/java/me/stivendarsi/orbit/orbit/menus/QuestMenu.java
@@ -3,9 +3,11 @@ package me.stivendarsi.orbit.orbit.menus;
 import io.papermc.paper.dialog.Dialog;
 import io.papermc.paper.registry.data.dialog.DialogBase;
 import io.papermc.paper.registry.data.dialog.body.DialogBody;
+import io.papermc.paper.registry.data.dialog.body.PlainMessageDialogBody;
 import io.papermc.paper.registry.data.dialog.type.DialogType;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.inventory.ItemType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,27 +32,33 @@ public class QuestMenu {
 
         DialogBody dailyQuest1 = DialogBody.plainMessage(mm.deserialize("" +
                 "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>להרוג שחקנים שונים</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
-                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
                 "<#fffb00>25/100</#fffb00><newline>" +
-                "<gradient:#e32a05:#ff4000:#e32a05>☐ עדיין לא הושלם</gradient:#e32a05:#ff4000:#e32a05>"));
+                "<gradient:#e32a05:#ff4000:#e32a05>☐ עדיין לא הושלם</gradient:#e32a05:#ff4000:#e32a05><newline>" +
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5>"
+        ));
 
         DialogBody dailyQuest2 = DialogBody.plainMessage(mm.deserialize("" +
                 "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>לחצוב 50 בלוקים</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
-                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
                 "<#fffb00>59/100</#fffb00><newline>" +
-                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55>"));
+                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55><newline>" +
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5>"
+        ));
 
         DialogBody dailyQuest3 = DialogBody.plainMessage(mm.deserialize("" +
                 "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>לקנות 1000 אייטמים מהחנות</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
-                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
                 "<#fffb00>6/100</#fffb00><newline>" +
-                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55>"));
+                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55><newline>"+
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5>"
+        ));
 
-        DialogBody dailyQuest4 = DialogBody.plainMessage(mm.deserialize("" +
+
+        PlainMessageDialogBody dailyQuest4text = DialogBody.plainMessage(mm.deserialize("" +
                 "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>לקנות 1000 אייטמים מהחנות</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
-                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
                 "<#fffb00>6/100</#fffb00><newline>" +
-                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55>"));
+                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55><newline>" +
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5>"
+        ), 170);
+        DialogBody dailyQuest4 = DialogBody.item(ItemType.EMERALD.createItemStack()).description(dailyQuest4text).build();
 
         DialogBody seasonQuests = DialogBody.plainMessage(mm.deserialize("<u><gradient:#e37602:#ffd500:#e37602>משימות עונתיות</gradient:#e37602:#ffd500:#e37602></u>"), 400);
 
@@ -63,5 +71,12 @@ public class QuestMenu {
         bodies.add(dailyQuest4);
         bodies.add(seasonQuests);
         return bodies;
+    }
+
+    private DialogBody getQuestBlock(String questText, int required, int completed, String rewardText){
+        List<String> builder = new ArrayList<>();
+
+        MiniMessage mm = MiniMessage.miniMessage();
+        builder.add( "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>לקנות 1000 אייטמים מהחנות</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>");
     }
 }
