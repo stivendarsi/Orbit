b49be23bdcfaf7fab0db3677d375c74b839e00ad
stiven <stivendaris9@gmail.com>
2026-07-01 00:29:50 +0300

    Patches

--- src/main/java/me/stivendarsi/orbit/orbit/menus/QuestMenu.java
+++ src/main/java/me/stivendarsi/orbit/orbit/menus/QuestMenu.java
@@ -2,15 +2,66 @@ package me.stivendarsi.orbit.orbit.menus;
 
 import io.papermc.paper.dialog.Dialog;
 import io.papermc.paper.registry.data.dialog.DialogBase;
+import io.papermc.paper.registry.data.dialog.body.DialogBody;
 import io.papermc.paper.registry.data.dialog.type.DialogType;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestMenu {
-    public Dialog getQuestDialog(){
+    public Dialog getQuestDialog() {
         Dialog questDialog = Dialog.create(b -> {
-            b.empty().type(DialogType.notice()).base(DialogBase.builder(Component.text("משימות")).externalTitle(Component.text("משימות")).build());
+            b.empty()
+                    .type(DialogType.notice())
+                    .base(DialogBase.builder(Component.text("משימות")).body(getBody())
+                            .externalTitle(Component.text("משימות")).build());
         });
 
         return questDialog;
+    }
+
+
+    private List<DialogBody> getBody() {
+        MiniMessage mm = MiniMessage.miniMessage();
+        DialogBody dailyQuests = DialogBody.plainMessage(mm.deserialize("<u><gradient:#2a94f7:#63cbff:#2a94f7>משימות יומיות</gradient:#2a94f7:#63cbff:#2a94f7></u>"), 400);
+
+
+        DialogBody dailyQuest1 = DialogBody.plainMessage(mm.deserialize("" +
+                "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>להרוג שחקנים שונים</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
+                "<#fffb00>25/100</#fffb00><newline>" +
+                "<gradient:#e32a05:#ff4000:#e32a05>☐ עדיין לא הושלם</gradient:#e32a05:#ff4000:#e32a05>"));
+
+        DialogBody dailyQuest2 = DialogBody.plainMessage(mm.deserialize("" +
+                "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>לחצוב 50 בלוקים</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
+                "<#fffb00>59/100</#fffb00><newline>" +
+                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55>"));
+
+        DialogBody dailyQuest3 = DialogBody.plainMessage(mm.deserialize("" +
+                "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>לקנות 1000 אייטמים מהחנות</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
+                "<#fffb00>6/100</#fffb00><newline>" +
+                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55>"));
+
+        DialogBody dailyQuest4 = DialogBody.plainMessage(mm.deserialize("" +
+                "<gray>⏵ <gradient:#fabf1b:#faea3c:#fabf1b>לקנות 1000 אייטמים מהחנות</gradient:#fabf1b:#faea3c:#fabf1b> ⏴<newline>" +
+                "<gradient:#d56cf5:#f59cff:#d56cf5>◆ 120 כוכבים ◆</gradient:#d56cf5:#f59cff:#d56cf5><newline>" +
+                "<#fffb00>6/100</#fffb00><newline>" +
+                "<gradient:#07ba55:#32f02b:#07ba55>☑ הושלם</gradient:#07ba55:#32f02b:#07ba55>"));
+
+        DialogBody seasonQuests = DialogBody.plainMessage(mm.deserialize("<u><gradient:#e37602:#ffd500:#e37602>משימות עונתיות</gradient:#e37602:#ffd500:#e37602></u>"), 400);
+
+
+        List<DialogBody> bodies = new ArrayList<>();
+        bodies.add(dailyQuests);
+        bodies.add(dailyQuest1);
+        bodies.add(dailyQuest2);
+        bodies.add(dailyQuest3);
+        bodies.add(dailyQuest4);
+        bodies.add(seasonQuests);
+        return bodies;
     }
 }
